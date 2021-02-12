b60c0fce9d8f86dd30d2d0ff49f95a143c2d5c75
brooke <brookedanae88@gmail.com>
2021-02-11 22:30:07 -0500

    Updated class

---
+++ Lab11/Lab11/Movie.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab11
+{
+    public class Movie
+    {
+        private string _category;
+        private string _title;
+
+        public Movie(string title, string category)
+        {
+            _title = title;
+            _category = category;
+        }
+
+        public Movie()
+        {
+
+        }
+
+        public void GetHorror()
+        {
+            var list = InitializeList();
+
+            foreach (var movie in list)
+            {
+                if (movie._category == "horror")
+                {
+                    Console.WriteLine(movie._title);
+                }
+            }
+        }
+
+        public void GetAnimated()
+        {
+            var list = InitializeList();
+
+            foreach (var movie in list)
+            {
+                if (movie._category == "animated")
+                {
+                    Console.WriteLine(movie._title);
+                }
+            }
+        }
+
+        public void GetSciFi()
+        {
+            var list = InitializeList();
+
+            foreach (var movie in list)
+            {
+                if (movie._category == "scifi")
+                {
+                    Console.WriteLine(movie._title);
+                }
+            }
+        }
+
+        public void GetDrama()
+        {
+            var list = InitializeList();
+
+            foreach (var movie in list)
+            {
+                if (movie._category == "drama")
+                {
+                    Console.WriteLine(movie._title);
+                }
+            }
+        }
+
+        private List<Movie> InitializeList()
+        {
+            var list = new List<Movie>();
+
+            list.Add(new Movie("Spirited Away", "animated"));
+            list.Add(new Movie("Frozen", "animated"));
+            list.Add(new Movie("Rambo", "drama"));
+            list.Add(new Movie("Django Unchained", "drama"));
+            list.Add(new Movie("Texas Chainsaw Massacre", "horror"));
+            list.Add(new Movie("Saw", "horror"));
+            list.Add(new Movie("Star Wars", "sci-fi"));
+            list.Add(new Movie("Jurassic", "sci-fi"));
+            list.Add(new Movie("Annabelle", "horror"));
+            list.Add(new Movie("The Conjouring", "horror"));
+
+            return list;
+        }
+    }
+}
--- Lab11/Lab11/Program.cs
+++ Lab11/Lab11/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lab11
 {
@@ -6,7 +7,32 @@ namespace Lab11
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            GetUserInput();
+
+        }
+        private static string GetUserInput()
+        {
+            string response;
+            do
+            {
+                Console.WriteLine("Welcome to the Movie List Application!" + "\r\n" + "There are 100 movies in this list." + "\r\n" +
+              "What category are you interested in?");
+                response = Console.ReadLine();
+                
+            } while (!IsUserInputValid(response));
+
+            return response;
+        }
+        private static bool IsUserInputValid(string response)
+        {
+            if (response == "Scifi" || response == "Horror" || response == "Animated" || response == "Drama")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
